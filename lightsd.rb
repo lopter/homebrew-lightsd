1e30807fd8a21aae90606ae434bd0ad960fbf8f5
Louis Opter <louis@uber.com>
2015-09-04 02:33:56 -0700

    Fix devel install

--- lightsd.rb
+++ lightsd.rb
@@ -5,7 +5,7 @@ class Lightsd < Formula
   homepage "https://github.com/lopter/lightsd/"
   url "https://api.github.com/repos/lopter/lightsd/tarball/0.9.1"
   sha256 "ef4f8056bf39c8f2c440e442f047cafce1c102e565bb007791a27f77588157c2"
-  revision 1
+  revision 2
 
   depends_on "cmake" => :build
   depends_on "libbsd" => :optional
@@ -25,6 +25,7 @@ class Lightsd < Formula
 
   devel do
     url "https://github.com/lopter/lightsd.git"
+    version "1.0"
   end
 
   test do
