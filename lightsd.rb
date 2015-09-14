1695a7e32d6f4e80f307746e96d3786e483ce6a5
Louis Opter <louis@uber.com>
2015-09-13 18:46:21 -0700

    lightsd 0.9.2, launchd integration, keep devel for myself for now

--- lightsd.rb
+++ lightsd.rb
@@ -3,28 +3,72 @@ require "formula"
 class Lightsd < Formula
   desc "Daemon to control your LIFX wifi smart bulbs"
   homepage "https://github.com/lopter/lightsd/"
-  url "https://github.com/lopter/lightsd/archive/0.9.1.tar.gz"
-  sha256 "72eba6074ed18609fb0caf7b7429e1b8f6c3564ca6f81357be22c06ac00956b6"
-  revision 3
+  url "https://github.com/lopter/lightsd/archive/0.9.2.tar.gz"
+  sha256 "2d785ede7c2e3b603ad33c11c3e44b5c634ee17e745d39296125025eddbbf420"
+  revision 1
 
   depends_on "cmake" => :build
-  depends_on "libbsd" => :optional
   depends_on "libevent" => :build
   depends_on "python" => :optional
 
   def install
-    args = %W[
-      -DCMAKE_BUILD_TYPE=RELEASE
-      -DCMAKE_INSTALL_PREFIX=#{prefix}
-    ]
+    args = std_cmake_args
+    args << "-DLGTD_RUNTIME_DIRECTORY=#{var}/run/lightsd"
 
     system "cmake", *args
-
     system "make", "install"
   end
 
-  devel do
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/lightsd</string>
+          <string>-f</string>
+          <string>-v</string>
+          <string>warning</string>
+          <string>-s</string>
+          <string>#{var}/run/lightsd/socket</string>
+          <string>-c</string>
+          <string>#{var}/run/lightsd/pipe</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/lightsd.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/lightsd.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  def caveats; <<-EOS.undent
+    Once you've started lightsd with launchctl load (see below), you can start
+    poking around with lightsc.py:
+
+      `lightsd --prefix`/share/doc/lightsd/examples/lightsc.py
+    EOS
+  end
+
+  head do
     url "https://github.com/lopter/lightsd.git"
+  end
+
+  devel do
+    url "file:///Users/louis/projs/lightsd", :using => :hg
     version "1.0"
   end
 
@@ -36,7 +80,6 @@ class Lightsd < Formula
         -c #{dir}/lightsd.cmd
         -h
       ]
-
       system "#{bin}/lightsd", *args
     end
   end
