fad09b350d90b112b2978df234adb2e13e99a900
Buildbot <buildbot@kalessin.fr>
2017-02-12 23:43:59 +0000

    New upstream release 1.2.1-rc.4

--- lightsd.rb
+++ lightsd.rb
@@ -3,10 +3,10 @@ require "formula"
 class Lightsd < Formula
   desc "Daemon to control your LIFX wifi smart bulbs"
   homepage "https://github.com/lopter/lightsd/"
-  url "https://downloads.lightsd.io/releases/lightsd-1.2.1-rc.3.tar.gz"
-  sha256 "2aa94c910e5581b427f80a2a95858c57c321b196921bdbdc945d996c9d325871"
+  url "https://downloads.lightsd.io/releases/lightsd-1.2.1-rc.4.tar.gz"
+  sha256 "b46401bc97bb3570c5ef72b0b1492eb74ced1c5b5c576ccb62d92a1f5da8980d"
   # This will have ~ instead of - for rc versions:
-  version "1.2.1~rc.3"
+  version "1.2.1~rc.4"
   revision 1
 
   depends_on "cmake" => :build
